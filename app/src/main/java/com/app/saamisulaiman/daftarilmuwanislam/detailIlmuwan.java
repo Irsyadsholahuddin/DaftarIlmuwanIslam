ac987c4112357bc2e87f7deca30e80ddb93ec58f
SaamiSulaiman <saamisulaiman@gmail.com>
2019-05-06 21:22:01 +0700

    inintial

---
+++ app/src/main/java/com/app/saamisulaiman/daftarilmuwanislam/detailIlmuwan.java
@@ -0,0 +1,40 @@
+package com.app.saamisulaiman.daftarilmuwanislam;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class detailIlmuwan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_ilmuwan);
+
+        Intent intent = getIntent();
+        String nama = intent.getStringExtra("putNama");
+        String url = intent.getStringExtra("putUrl");
+
+        getSupportActionBar().setTitle("Biografi "+ nama);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        WebView webView = findViewById(R.id.detailWebView);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
